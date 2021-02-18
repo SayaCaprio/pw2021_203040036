46a837ef34e1205901e9100820f39e80b6877bf0
sayacaprio <caprio.sinaga16@gmail.com>
2021-02-18 14:24:49 +0700

    menyelesaikan pertemuan 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,89 @@
+<?php
+/* Caprio Sinaga 
+203040036
+https://github.com/SayaCaprio/pw2021_203040036.git
+pertemuan 2 - 8 februari 
+mempelajari sintaks php */
+
+    // pertemuan 2 - php dasar
+    //sintaks php
+
+    //standar output
+    // echo, print
+    // print_r
+    // var_dump
+
+
+    //penulisan sintaks PHP
+    //1. PHP di dalam HTML
+    //2. HTML di dalam PHP 
+    //variable dan tipe data
+    //variable tidak boleh diawali dengan angka, tapi boleh mengandung angka
+
+
+    // $nama = "Caprio sinaga";
+    // echo "halo, nama saya $nama";
+
+
+    //operator
+    // aritmatika
+    
+    // + - * / %
+    // $x = 10;
+    // $y = 20;
+    // echo $x * $y;
+
+
+    //penggabung string/ concatrtion/ concat
+    //.
+
+    // $nama_depan = "Caprio";
+    // $nama_belakang ="sinaga";
+
+    // echo $nama_depan . " " . $nama_belakang;
+
+
+    //assigment 
+    //=, +=, -=, *=, %=, .=
+
+    // $x = 1;
+    // $x -= 5;
+
+    // echo $x;
+
+    // $nama ="caprio";
+    // $nama .= " ";
+    // $nama .="Sinaga";
+
+    // echo $nama;
+
+    //perbandingan
+    // <, >, <=, >=, ==. :=
+
+    
+
+    //identitas
+    //===, !===
+
+    // var_dump (1 === "1");
+
+    //logika
+    // &&, ||, ! 
+
+    $x = 30;
+    var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
